238e4a28466434819786d1b17a8b05b5c1f8f72b
APC9 <penacastilloalberto32@gmail.com>
2024-02-15 22:00:40 +0100

    test

--- electronic-bank/src/main/java/com/electronic/bank/electronic_bank/ElectronicBankApplication.java
+++ electronic-bank/src/main/java/com/electronic/bank/electronic_bank/ElectronicBankApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ElectronicBankApplication {
 
+	public static String name = "alberto";
+
 	public static void main(String[] args) {
 		SpringApplication.run(ElectronicBankApplication.class, args);
+		System.out.println(name);
 	}
 
 } 
