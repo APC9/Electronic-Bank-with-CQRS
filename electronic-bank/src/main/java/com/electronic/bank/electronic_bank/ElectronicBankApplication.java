63b33e752c5857da82023f0fe1fd85f0bdc584ad
APC9 <penacastilloalberto32@gmail.com>
2024-02-15 22:48:23 +0100

    test.

--- electronic-bank/src/main/java/com/electronic/bank/electronic_bank/ElectronicBankApplication.java
+++ electronic-bank/src/main/java/com/electronic/bank/electronic_bank/ElectronicBankApplication.java
@@ -10,7 +10,7 @@ public class ElectronicBankApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ElectronicBankApplication.class, args);
-		System.out.println(name);
+		System.out.println("Hola "+name);
 	}
 
 } 
